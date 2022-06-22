8a3a09d50a8cfcbfd1de9f82d8ac107bc5d19233
pixies0 <jpcunha5@gmail.com>
2022-06-22 17:22:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('templates/header.php');
 <div id="bb1" style="min-height: 100vh; width:100%; padding: 150px; background-image: url('assets/images/bg_index2.png'); background-position: center; background-repeat: no-repeat; background-size: cover;">
     <div class="row">
         <div class="col-lg-6">
-            <img src="assets/images/Logo_UFT.png" width="156px" height="107px">
+            <img src="assets/images/logo_UFT.png" width="156px" height="107px">
             <br><br>
             <h1 class="display-4" style="font-weight:bold !important; font-size: 48px; font-family: 'Roboto', sans-serif; color: White; -webkit-text-stroke-width: 0.75px;
     -webkit-text-stroke-color: Black;"> Sua Digital Library ! </h1>
